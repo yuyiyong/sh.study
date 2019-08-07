a8fa32e2fea3ba6c5df1d3d991908337566df2b1
yuyiyong <1191893756@qq.com>
2019-08-08 07:23:24 +0800

    19-8-8 修改react-redux

---
+++ build/precache-manifest.e4d563ee2f66e71646191331afa011bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49263c784855591dcbbebb0bc693122d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda62e6bc3b8568638d2",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "8d30bfff28bb2de60318",
+    "url": "/static/css/main.5fd9c300.chunk.css"
+  },
+  {
+    "revision": "fda62e6bc3b8568638d2",
+    "url": "/static/js/2.046c0633.chunk.js"
+  },
+  {
+    "revision": "8d30bfff28bb2de60318",
+    "url": "/static/js/main.30d57f43.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc847ae737cd39480906724daec6bbbf",
+    "url": "/static/media/20190704161442_63cf24ba219fa956 (1).dc847ae7.jpg"
+  }
+]);
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -7,18 +7,24 @@ import TodoList from "./component/todoList/TodoList";
 import Layout from "./component/xunlei/layout/layout";
 import List from "./component/router/list";
 import Login from "./component/login/login";
+import {Provider} from 'react-redux';
+import store from "./store";
 
 /*function Index() {
     return <h2>dUI</h2>;
 }*/
-
+const App = () => (
+    <Provider store={store}>
+        <TodoList />
+    </Provider>
+)
 
 function AppRouter() {
     return (
         <Router>
             <Layout>
                 <Route path="/" exact component={Index}/>
-                <Route path="/todoList/" component={TodoList}/>
+                <Route path="/todoList/" component={App}/>
                 <Route path="/list/:id" component={List} />
                 <Route path="/login" component={Login} />
             </Layout>
