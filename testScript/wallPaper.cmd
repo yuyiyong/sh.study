71897bff0a75a154ef7aaa26826fcb2b155ae4eb
yuyiyong <1191893756@qq.com>
2019-08-29 07:09:43 +0800

    壁纸cmd

--- testScript/wallPaper.cmd
+++ testScript/wallPaper.cmd
@@ -7,9 +7,9 @@
 @ECHO                          3
 @ECHO           *********************************************************************************************************
 
-copy C:\Users\dui\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\* C:\wallPaper
+copy C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\* C:\wallPaper
 Ren C:\wallPaper\*.* *.png
-explorer C:\Users\dui\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\
+explorer C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\
 explorer C:\wallPaper
 
 @ECHO success
