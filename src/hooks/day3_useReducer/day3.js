3bc1eff3d3e9d494c7a23d51eb22ebae1f743247
yuyiyong <1191893756@qq.com>
2019-08-31 08:05:46 +0800

    自定义hocks

--- src/hooks/day3_useReducer/day3.js
+++ src/hooks/day3_useReducer/day3.js
@@ -3,17 +3,22 @@ import UseReducerss from "./userReducers/useReducers";
 import UseMemos from "./useMemo/useMemos";
 import {Row, Col} from "antd"
 import UseRefs from "./useRef/useRefs";
+import CustomHockes from "./customHokes/customHokes";
 
 
 const Day3 = () => {
     return (
         <div>
             <Row>
-                <Col span={8}> <UseReducerss /></Col>
-                <Col span={8}>  <UseMemos/></Col>
-                <Col span={8}>  <UseRefs/></Col>
+                <Col span={8}> <UseReducerss/></Col>
+                <Col span={8}> <UseMemos/></Col>
+                <Col span={8}> <UseRefs/></Col>
             </Row>
-
+            <Row>
+                <Col span={8}><CustomHockes/></Col>
+                <Col span={8}></Col>
+                <Col span={8}></Col>
+            </Row>
 
         </div>
     )
